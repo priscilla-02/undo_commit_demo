9c2d1f176832d4b23d517b5cd78bbd6970121b0b
priscilla-02 <priscilla.chankwaitung@gmail.com>
2024-11-11 12:05:26 +0000

    update: Commit G

--- my-solid-app/src/App.tsx
+++ my-solid-app/src/App.tsx
@@ -14,6 +14,7 @@ const App: Component = () => {
         <div class={styles.box2}>Box D</div>
         <div class={styles.box2}>Box E</div>
         <div class={styles.box2}>Box F</div>
+        <div class={styles.box2}>Box G</div>
       </div>
     </div>
   );
